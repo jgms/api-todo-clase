d8e7b420405b2c17489085d4184538b4143f899f
Joaquín <joaquinmendozasebastian@gmail.com>
2024-02-15 12:48:27 +0100

    endpoints básicos y entorno de pruebas

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const express = require("express");
 
 const servidor = express();
 
+servidor.use("/pruebas",express.static("./pruebas_api"));
 
+servidor.get("/api-todo", (peticion,respuesta) => {
+    respuesta.send("método GET");
+});
 
+servidor.post("/api-todo", (peticion,respuesta) => {
+    respuesta.send("método POST");
+});
+
+servidor.put("/api-todo", (peticion,respuesta) => {
+    respuesta.send("método PUT");
+});
+
+servidor.delete("/api-todo", (peticion,respuesta) => {
+    respuesta.send("método DELETE");
+});
+
+servidor.use((peticion,respuesta) => {
+    respuesta.json({ error : "not found" });
+});
 
 
 servidor.listen(process.env.PORT);
