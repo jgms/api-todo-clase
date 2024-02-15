78ee04ec40a06c300f108d35d41f18826e1ea01a
Joaquín <joaquinmendozasebastian@gmail.com>
2024-02-15 14:54:34 +0100

    mitasd de validación de errores POST

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 require("dotenv").config();
 const express = require("express");
+const {getTareas,crearTarea} = require("./db");
+const {json} = require("body-parser");
 
 const servidor = express();
 
+servidor.use(json());
+
 servidor.use("/pruebas",express.static("./pruebas_api"));
 
-servidor.get("/api-todo", (peticion,respuesta) => {
-    respuesta.send("método GET");
+servidor.get("/api-todo", async (peticion,respuesta) => {
+    try{
+        let tareas = await getTareas();
+
+        respuesta.json(tareas);
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json(error);
+    }
 });
 
-servidor.post("/api-todo", (peticion,respuesta) => {
-    respuesta.send("método POST");
+servidor.post("/api-todo/crear", async (peticion,respuesta,siguiente) => {
+    
+    let {tarea} = peticion.body;
+
+    if(tarea && tarea.trim() != ""){
+       return respuesta.send("método POST");
+    }
+
+    siguiente("...no me enviaste tarea");
+    
 });
 
 servidor.put("/api-todo", (peticion,respuesta) => {
@@ -23,6 +42,10 @@ servidor.delete("/api-todo", (peticion,respuesta) => {
 
 servidor.use((peticion,respuesta) => {
     respuesta.json({ error : "not found" });
+});
+
+servidor.use((error,peticion,respuesta,siguiente) => {
+    respuesta.send("..error");
 });
 
 
